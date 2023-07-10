328b9cb8f0a099248b6cb68e22369a08c65aa024
Jiwonii97 <wldnjs3633@naver.com>
2023-07-11 01:31:00 +0900

    230705 코테스터디 문제풀이

---
+++ jiwon/baek_4659.py
@@ -0,0 +1,20 @@
+# https://www.acmicpc.net/problem/4659
+# 비밀번호 발음하기
+# 정규식 활용 / 아이디어 활용
+
+import sys
+import re
+
+while True:
+    text = sys.stdin.readline().strip()
+    if text == "end":     # 종료조건
+        break
+
+    case1 = re.findall(r"a|e|i|o|u", text)
+    case2 = re.findall(r"([a|e|i|o|u]{3})|([^a|e|i|o|u]{3})", text)
+    case3 = re.findall(r"([a-df-np-z])\1", text)
+
+    if len(case1) != 0 and len(case2) == 0 and len(case3) == 0:
+        print(f"<{text}> is acceptable.")
+    else:
+        print(f"<{text}> is not acceptable.")
