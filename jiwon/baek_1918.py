d4a3ac6995eaab2186579180c41696807e5c1d5d
jiwonii97 <wldnjs3633@naver.com>
2023-08-07 19:29:39 +0900

    230714 코테스터디 문제풀이

--- jiwon/baek_1918.py
+++ jiwon/baek_1918.py
@@ -4,3 +4,28 @@
 import sys
 
 formula = sys.stdin.readline().strip()
+
+st, result = [], ""
+for f in formula:
+    if f.isalpha():     # 문자면 일단 스택에 넣기
+       result += f
+    else:
+        if f == '(':
+            st.append(f)
+        elif f == '*' or f == '/':
+            while st and (st[-1] == '*' or st[-1] =='/'):     # 우선순위가 높은 기호는 모두 추가
+                result += st.pop()
+            st.append(f)
+        elif f == '+' or f == '-':
+            while st and st[-1] != '(':     # 우선순위가 높은 기호는 모두 추가
+                result+= st.pop()
+            st.append(f)
+        elif f == ')':
+            while st and st[-1] != '(':     # 괄호안에 있는 모든 기호를 표현
+                result += st.pop()
+            st.pop()
+
+# 남은 기호들 모두 역순으로 넣기
+result += ''.join(st[::-1])
+    
+print(result)
