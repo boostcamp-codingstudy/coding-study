9b416ec17f1ec8c2d92abad3b08cf474405ec852
unknown <eogml88@gmail.com>
2023-09-21 21:25:05 +0900

    to add a solution.

--- daehee/BJ16931.py
+++ daehee/BJ16931.py
@@ -1,4 +1,8 @@
+# standard solution
 *A,=map(int,open(0).read().split())
-n,m=A[:2];A=A[2:];s=2*n*m
-k=max(A)
-print(2*(n*m+n*k+m*k))
+n,m=A[:2];A=A[2:];s=2*n*m+4*sum(A)
+for r in range(n):
+    for c in range(m):
+        if r:s-=2*min(A[r*m+c],A[r*m-m+c])
+        if c:s-=2*min(A[r*m+c],A[r*m+c-1])
+print(s)
